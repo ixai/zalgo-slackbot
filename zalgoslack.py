482dfe1e12c2f8a08e143e5b890efad5237f882c
Ixai Lanzagorta <ixai@ooyala.com>
2015-08-04 12:38:59 -0500

    Setup zalgo Flask app

---
+++ zalgoslack.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, abort
+from zalgo import zalgo
+from config import TOKEN
+import requests
+
+URL = "https://slack.com/api"
+CHAT_POST = "chat.postMessage"
+INTENSITY = { "up": 5, "mid": 5, "down": 5}
+
+app = Flask(__name__)
+
+@app.route("/zalgify", methods=["POST"])
+def zalgify():
+    try:
+        channel = request.form["channel"]
+        text = request.form["text"]
+    except:
+        abort(400)
+    zalgd = zalgo(text, INTENSITY)
+    params = {
+        "token": TOKEN,
+        "channel": channel,
+        "text": zalgd,
+    }
+    r = requests.post("{}/{}".format(URL, CHAT_POST), data=params)
+    if r.status_code != 200:
+        abort(500)
+    return zalgd
+
+if __name__ == "__main__":
+    app.run()
